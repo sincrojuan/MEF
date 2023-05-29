349dda90ab5ea2b3e111bc3e62fa1a702d43cd2d
Tomás Brancatisano <109698513+tomasbranca@users.noreply.github.com>
2023-05-28 23:04:16 -0300

    Codigo base completo
    No es el codigo final

--- src/main.cpp
+++ src/main.cpp
@@ -1,144 +1,351 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include <TinyGPSPlus.h>
 
-//pines definidos:
-#define ReleMotor 5       //Rele(NC) que controla la alimentacion del CDI de la moto
-#define ReleSistema 6     //Rele(NA) que controla la alimentacion de los modulos
+// pines definidos:
+#define ReleMotor 5   // Rele(NC) que controla la alimentacion del CDI de la moto
+#define ReleSistema 6 // Rele(NA) que controla la alimentacion de los modulos
 #define PulsCte 2
-SoftwareSerial BtModule(10, 11); //10 Rx, 11 Tx
-SoftwareSerial GSMModule(3, 4);
+SoftwareSerial BtModule(10, 11); // 10 Rx, 11 Tx
+SoftwareSerial GSMModule(3, 4);  // 3 Rx, 4 Tx
+SoftwareSerial GPSModule(8, 9);
+TinyGPSPlus gpsModule;
 
-//Estados Posibles (MEF):
-typedef enum{EstadoDesactivado, EstadoEspera, LecturaBT, LecturaSMS} estadoMEF;
+// Estados Posibles (MEF):
+typedef enum
+{
+  EstadoDesactivado,
+  EstadoEspera,
+  LecturaBT,
+  LecturaSMS
+} estadoMEF;
 estadoMEF EstadoActual;
 
-//variables:
+// variables:
 volatile unsigned int Milisegundos = 0, Segundos = 0;
 unsigned short int DatoBT;
 
-void setup() {
-  //Estado Pines:
+// Variables de SMS
+String numTelefono;
+String numeroPrincipal = "+5491167900710";
+String numerosAux[5];
+int numAux = 0;
+String pw = "12345";
+bool waitOp = false;
+String msj;
+bool ahorroFlag = false;
+
+void setup()
+{
+  // Estado Pines:
   pinMode(ReleMotor, OUTPUT);
   pinMode(ReleSistema, OUTPUT);
   pinMode(PulsCte, INPUT);
 
-  //Interrupción externa:
+  // Interrupción externa:
   attachInterrupt(digitalPinToInterrupt(PulsCte), Interrumpido, RISING);
 
-  //Timer setup:
-  SREG = (SREG & 0b01111111); //deshabilita la interrupción
-  TIMSK2 = TIMSK2 | 0b00000001; //habilita inte. por desbordamiento
-  TCCR2B = 0b00000011; //prescaler de 32: 8M/32=250k -> T=4u; 256 x 4u = 1.025ms
+  // Timer setup:
+  SREG = (SREG & 0b01111111);   // deshabilita la interrupción
+  TIMSK2 = TIMSK2 | 0b00000001; // habilita inte. por desbordamiento
+  TCCR2B = 0b00000011;          // prescaler de 32: 8M/32=250k -> T=4u; 256 x 4u = 1.025ms
   SREG = (SREG | 0b10000000);
 
-  //Inicializa Serial(es)
+  // Inicializa Serial(es)
+  Serial.begin(9600);
   BtModule.begin(38400);
   GSMModule.begin(9600);
+  GPSModule.begin(9600);
+
+  delay(2000); // Espera que los modulos inicien correctamente
+
+  // Configuracion de GSM
+  GSMModule.println("AT+CMGF=1"); // Setea el GSM en modo texto
+  delay(100);
+  GSMModule.println("AT+CNMI=2,2,0,0,0"); // Envia los SMS al puerto serie
+  delay(100);
 }
 
-void loop() {
-  switch(EstadoActual) {
-    case EstadoDesactivado:
+void loop()
+{
+  switch (EstadoActual)
+  {
+  case EstadoDesactivado:
+    ActivarMotor();
+    digitalWrite(ReleSistema, LOW);
 
-      ActivarMotor();
-      digitalWrite(ReleSistema, LOW);
+    if (Segundos >= 15)
+    {
+      Segundos = 0;
 
-      if(Segundos >= 15){
-        Segundos = 0;
-
-        if(BtModule.available()){
-          DatoBT = BtModule.read();
-          EstadoActual = LecturaBT;
-        }
-
+      if (BtModule.available())
+      {
+        DatoBT = BtModule.read();
+        EstadoActual = LecturaBT;
       }
+    }
     break;
 
-    case EstadoEspera:
+  case EstadoEspera:
+    if (Segundos >= 15)
+    {
+      Segundos = 0;
 
-      if(Segundos >= 15){
-        Segundos = 0;
-
-        if(BtModule.available()){
-          DatoBT = BtModule.read();
-          EstadoActual = LecturaBT;
-        }
-        else if(GSMModule.available())
-          EstadoActual = LecturaSMS;
-    
+      if (BtModule.available())
+      {
+        DatoBT = BtModule.read();
+        EstadoActual = LecturaBT;
       }
-    break;
-
-    case LecturaBT:
-
-      switch(DatoBT){
-        case 0:
-          EstadoActual = EstadoDesactivado;
-        break;
-
-        case 1:
-          ActivarSistema();
-        break;
-
-        case 2:
-          ActivarMotor();
-          EstadoActual = EstadoEspera;
-        break;
-        
-        case 3:
-          ModoAhorro();
-          //EstadoActual = EstadoEspera;
-        break;
+      else if (GSMModule.available())
+      {
+        EstadoActual = LecturaSMS;
       }
+    }
     break;
 
-    case LecturaSMS:
-      //¿?
-    break;
-
-    default:
+  case LecturaBT:
+    switch (DatoBT)
+    {
+    case 0:
       EstadoActual = EstadoDesactivado;
-    break; 
+      break;
+
+    case 1:
+      ActivarSistema();
+      break;
+
+    case 2:
+      ActivarMotor();
+      EstadoActual = EstadoEspera;
+      break;
+
+    case 3:
+      ModoAhorro();
+      // EstadoActual = EstadoEspera;
+      break;
+    }
+    break;
+
+  case LecturaSMS:
+    msj = GSMModule.readString();
+    if (msj.indexOf("+") != -1)
+    {
+      numTelefono = obtenerNumTelefono(msj); // Verifica si el SMS tiene un numero telefonico
+
+      // Verifica si el numero principal
+      if (numTelefono == numeroPrincipal)
+      {
+        enviarSMS(numTelefono, menuOptions());
+        waitOp = true;
+      }
+
+      // Verifica si es algun numero auxiliar
+      else if (verifNumSec(numTelefono))
+      {
+        enviarSMS(numTelefono, menuOptions());
+        waitOp = true;
+      }
+
+      // Verifica si el SMS enviado es la contraseña de activacion
+      else if (msj.indexOf(pw) != -1)
+      {
+        guardarNumAux(numTelefono);
+        enviarSMS(numTelefono, "Número auxiliar guardado.");
+        enviarSMS(numTelefono, menuOptions());
+        waitOp = true;
+      }
+
+      // No cumplío ningun requisito
+      else
+      {
+        enviarSMS(numTelefono, "No se ha podido registrar este número.");
+      }
+    }
+    // Selecciona el modo
+    else if (waitOp)
+    {
+      int op = msj.toInt();
+      selectOp(numTelefono, op);
+      waitOp = false;
+    }
+    break;
+
+  default:
+    EstadoActual = EstadoDesactivado;
+    break;
   }
 }
 
+// funciónes de acciones/comandos
 
-//funciónes de acciones/comandos
-void ActivarSistema(){
-  EstadoActual = EstadoEspera;
-  digitalWrite(ReleSistema, HIGH);
-}
-void DeactivarSistema(){
-  EstadoActual = EstadoDesactivado;
-  digitalWrite(ReleSistema, LOW);
-}
-
-void ActivarMotor(){
-  digitalWrite(ReleMotor, LOW);
-}
-void DeactivarMotor(){
-  digitalWrite(ReleMotor, HIGH);
+// Obtiene el numero de telefono del SMS
+String obtenerNumTelefono(String msj)
+{
+  int inicio = msj.indexOf("+");
+  int fin = msj.indexOf("/", inicio);
+  String numTelefono = msj.substring(inicio, fin);
+  return numTelefono;
 }
 
-void EnviarUbicación(){
-  //no se cómo se hace xd
+// Recorre array de numeros secundarios
+bool verifNumSec(String numTelefono)
+{
+  for (int i = 0; i < numAux; i++)
+  {
+    if (numTelefono == numerosAux[i])
+    {
+      return true;
+    }
+  }
+  return false;
 }
 
-void ModoAhorro(){
-  //
+// Guarda numero auxiliar en el array de numeros secundarios
+void guardarNumAux(String numTelefono)
+{
+  if (numAux < 5)
+  {
+    numerosAux[numAux] = numTelefono;
+  }
+  numAux++;
 }
 
+// Despliega el menú de opciones
+String menuOptions()
+{
+  String menu = "Menú de opciones:\n\n";
+  menu += "1. Activar/Desactivar Sistema\n";
+  menu += "2. Activar/Desactivar Motor\n";
+  menu += "3. Activar/Desactivar modo ahorro\n";
+  menu += "4. Enviar Ubicación\n";
+}
 
-//función interrupción(desbordamiento):
-ISR(TIMER_OVF_vect){
+// Seleccionador (no se si esta bien dicho xd) de funciones
+void selectOp(String numTelefono, int op)
+{
+  switch (op)
+  {
+  case 1:
+    ActivarSistema();
+    break;
+
+  case 2:
+    ActivarMotor();
+    break;
+
+  case 3:
+    ModoAhorro();
+    break;
+
+  case 4:
+    EnviarUbicación();
+    break;
+
+  default:
+    enviarSMS(numTelefono, "Opción Inválida");
+    break;
+  }
+}
+
+// Enviador(????) de SMS
+void enviarSMS(String numTelefono, String msj)
+{
+  GSMModule.println("AT+CMGF=1");
+  delay(100);
+  GSMModule.println("AT+CMGS=\"" + numTelefono + "\"");
+  delay(100);
+  GSMModule.print(msj);
+  delay(100);
+  GSMModule.println((char)26); // Envía el carácter CTRL+Z
+  delay(100);
+}
+
+// Funcion para activar o desactivar el sistema
+void ActivarSistema()
+{
+  if (EstadoActual == EstadoEspera && digitalRead(ReleSistema) == HIGH)
+  {
+    EstadoActual = EstadoDesactivado;
+    digitalWrite(ReleSistema, LOW);
+    enviarSMS(numTelefono, "Se ha desactivado el sistema.");
+  }
+  else if (EstadoActual == EstadoDesactivado && digitalRead(ReleSistema) == LOW)
+  {
+    EstadoActual = EstadoEspera;
+    digitalWrite(ReleSistema, HIGH);
+    enviarSMS(numTelefono, "Se ha activado el sistema.");
+  }
+}
+
+// Funcion para activar o desactivar el motor
+void ActivarMotor()
+{
+  if (digitalRead(ReleMotor) == HIGH)
+  {
+    digitalWrite(ReleMotor, LOW);
+    enviarSMS(numTelefono, "Se ha desactivado el motor.");
+  }
+  else
+  {
+    digitalWrite(ReleMotor, HIGH);
+    enviarSMS(numTelefono, "Se ha activado el sistema.");
+  }
+}
+
+// Funcion para el envío del link de la ubicacion
+void EnviarUbicación()
+{
+  if (gpsModule.location.isValid() && gpsModule.location.isUpdated())
+  {
+    String ubicacion = obtenerLink(gpsModule.location.lat(), gpsModule.location.lng());
+    enviarSMS(numTelefono, ubicacion);
+  }
+  else
+  {
+    enviarSMS(numTelefono, "No se ha podido obtener la ubicación.");
+  }
+}
+
+// Funcion
+void ModoAhorro()
+{
+  if (!ahorroFlag)
+  {
+    // Desactivar modo ahorro de energía
+    GSMModule.println("AT+CFUN=1"); // Desactivar modo ahorro
+    delay(100);
+    enviarSMS(numTelefono, "Se ha desactivado el modo ahorro.");
+    ahorroFlag = true;
+  }
+  else
+  {
+    // Activar modo ahorro de energía
+    GSMModule.println("AT+CFUN=0"); // Activar modo ahorro
+    delay(100);
+    enviarSMS(numTelefono, "Se ha activado el modo ahorro.");
+    ahorroFlag = false;
+  }
+}
+
+String obtenerLink(float latitud, float longitud)
+{
+  String enlace = "https://maps.google.com/?q=" + String(latitud, 6) + "," + String(longitud, 6);
+  return enlace;
+}
+
+// función interrupción(desbordamiento):
+ISR(TIMER_OVF_vect)
+{
   Milisegundos++;
 
-  if(Milisegundos = 1000){
+  if (Milisegundos = 1000)
+  {
     Segundos++;
     Milisegundos = 0;
   }
 }
-//función interrupción(externa):
-void Interrumpido() {
+// función interrupción(externa):
+void Interrumpido()
+{
   EstadoActual = EstadoDesactivado;
 }
